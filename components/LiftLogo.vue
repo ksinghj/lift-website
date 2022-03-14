2426c51e12b1616e52ec1ef87c685fa6c51e8e2f
ksinghj <kartarsjabanda@gmail.com>
2022-03-14 16:55:09 +0000

    first commit & initial project setup

---
+++ components/LiftLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex">
+    <img class="w-25" src="../static/logo-dark.png">
+    <div>
+      <button>Try for free</button>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <LiftLogo />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import LiftLogo from '~/components/LiftLogo.vue'
+
+export default Vue.extend({
+  name: 'HomePage',
+  components: { LiftLogo }
+})
+</script>
